c4849b3838ab5b95b4886ea33fc4080873216294
natalvidesneto <natalvides.neto.01@gmail.com>
2024-07-07 16:42:37 -0300

    01

---
+++ DIO - Formação JavaScript Developer/02-JavaScript para Web - Fundamentos de HTML,CSS e APIs na Prática/01-Introdução ao Desenvolvimento Web com JavaScript/Página-01.js
@@ -0,0 +1,9 @@
+//Configurando o Ambiente
+/**
+ * Instalando a biblioteca:
+ * npm install -g http-server
+ * 
+ * Inciando :
+ * http-server ./ --> neste arquivo
+ * http-server "local do aquivo"
+ */
